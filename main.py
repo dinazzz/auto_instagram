5816791baad08aef84794f89f7e1984445cefcb1
dinaz <dinazval@gmail.com>
2019-04-18 14:32:08 +0500

    Завершены шаги 9-10

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def load_image(url, directory, filename):
         file.close()
 
 
+def return_extension(url):
+    return url.split('/')[-1].split('.')[-1]
+
+
 def fetch_spacex_last_launch(directory_for_loading):
     url_space_x = 'https://api.spacexdata.com/v3/launches/latest'
     response = requests.get(url_space_x)
@@ -22,7 +26,16 @@ def fetch_spacex_last_launch(directory_for_loading):
             load_image(link, directory_for_loading, filename)
 
 
+def fetch_hubble_images(image_id, directory):
+    url_hubble_api = 'http://hubblesite.org/api/v3/image/{}'.format(image_id)
+    response = requests.get(url_hubble_api)
+    if response.ok:
+        link_to_image = response.json()['image_files'][-1]['file_url']
+        file_extension = return_extension(link_to_image)
+        load_image(link_to_image, directory, '{}.{}'.format(image_id, file_extension))
+
+
 if __name__ == '__main__':
     directory_for_images = 'images'
-    fetch_spacex_last_launch(directory_for_images)
-
+    # fetch_spacex_last_launch(directory_for_images)
+    fetch_hubble_images(1, directory_for_images)
